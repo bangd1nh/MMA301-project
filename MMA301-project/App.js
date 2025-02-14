0b76eca1ca1d8b8ab9ce68010fe8ead76706f427
bangd1nh <dinhbang121@gmail.com>
2025-02-14 23:44:42 +0700

    trang chu

--- MMA301-project/App.js
+++ MMA301-project/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Trangchu from "./screen/trangchu";
+import { ScrollView } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <Trangchu></Trangchu>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flexGrow: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+    },
 });
