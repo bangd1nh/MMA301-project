ebec3b763226d140c2e9356f02a4341f7d6a9829
Duc Anh <anhduong764027@gmail.com>
2025-02-16 19:47:53 +0700

    profile users

--- MMA301-project/App.js
+++ MMA301-project/App.js
@@ -2,19 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Trangchu from "./screen/trangchu";
 import { ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ProfileScreen from "./screen/profileScreen";
 
+const Stack = createStackNavigator();
 export default function App() {
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Trangchu></Trangchu>
-        </ScrollView>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        backgroundColor: "#fff",
-        justifyContent: "center",
-    },
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
 });
