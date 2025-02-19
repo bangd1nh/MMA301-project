0c02521defa8845db3d61cf473b388a9890f76f3
bangd1nh <dinhbang121@gmail.com>
2025-02-19 10:48:36 +0700

    detail

--- MMA301-project/App.js
+++ MMA301-project/App.js
@@ -2,29 +2,53 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Trangchu from "./screen/trangchu";
 import { ScrollView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "./screen/profileScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+    createStaticNavigation,
+    NavigationContainer,
+    useNavigation,
+} from "@react-navigation/native";
+import Detail from "./screen/detail";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator({
+    screens: {
+        Home: Trangchu,
+        Profile: ProfileScreen,
+    },
+});
+
+const Navigation = createStaticNavigation(Drawer);
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile">
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen
+                    name="Home"
+                    component={Trangchu}
+                    options={{ headerShown: false }}
+                />
+                <Drawer.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{ headerShown: false }}
+                />
+                <Drawer.Screen
+                    name="Detail"
+                    component={Detail}
+                    options={{ headerShown: false }}
+                />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
+    container: {
+        flexGrow: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+    },
 });
--- MMA301-project/constant/data.js
+++ MMA301-project/constant/data.js
@@ -2,28 +2,59 @@ export const shoes = [
     {
         image: require("../assets/jordan1.jpg"),
         name: "Jordan 1",
+        brand: "Nike",
+        status: "Brand New",
         price: 180,
+        size: [7, 8, 9, 10, 11],
+        description:
+            "The iconic Air Jordan 1, a timeless classic that revolutionized the sneaker world.",
+        imgList: [
+            require("../assets/jordan1a.webp"),
+            require("../assets/jordan1b.png"),
+            require("../assets/jordan1c.jpg"),
+            require("../assets/jordan1d.webp"),
+        ],
     },
     {
         tag: "HOT",
         image: require("../assets/airmax90.png"),
         name: "Air Max 90",
+        brand: "Nike",
+        status: "Brand New",
         price: 120,
+        size: [6, 7, 8, 9, 10],
+        description:
+            "The Air Max 90, known for its comfort and style, is a must-have for sneaker enthusiasts.",
     },
     {
         image: require("../assets/ultraboost.webp"),
         name: "Ultra Boost",
+        brand: "Adidas",
+        status: "Brand New",
         price: 180,
+        size: [8, 9, 10, 11, 12],
+        description:
+            "The Ultra Boost offers unparalleled comfort and performance, perfect for running and casual wear.",
     },
     {
         tag: "NEW",
         image: require("../assets/converse.webp"),
         name: "Converse Chuck Taylor",
+        brand: "Converse",
+        status: "Brand New",
         price: 60,
+        size: [6, 7, 8, 9, 10],
+        description:
+            "The Converse Chuck Taylor, a timeless sneaker that has been a staple in fashion for decades.",
     },
     {
         image: require("../assets/vans.webp"),
         name: "Vans Old Skool",
+        brand: "Vans",
+        status: "Brand New",
         price: 50,
+        size: [7, 8, 9, 10, 11],
+        description:
+            "The Vans Old Skool, a classic skate shoe that combines style and durability.",
     },
 ];
