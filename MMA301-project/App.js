6ed1b7550599072fb52e41207867c4f556cea5eb
bangd1nh <dinhbang121@gmail.com>
2025-02-20 09:44:37 +0700

    a

--- MMA301-project/App.js
+++ MMA301-project/App.js
@@ -1,60 +1,3 @@
-// import { StatusBar } from "expo-status-bar";
-// import { Linking, StyleSheet, Text, View } from "react-native";
-// import Trangchu from "./screen/trangchu";
-// import { ScrollView } from "react-native";
-// import { createStackNavigator } from "@react-navigation/stack";
-// import LoginScreen from "./screen/LoginScreen";
-// import RegisterScreen from "./screen/RegisterScreen";
-// import ProfileScreen from "./screen/profileScreen";
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-// import {
-//     createStaticNavigation,
-//     NavigationContainer,
-//     useNavigation,
-// } from "@react-navigation/native";
-// import Detail from "./screen/detail";
-
-// const Stack = createStackNavigator();
-// const Drawer = createDrawerNavigator({
-//     screens: {
-//         Home: Trangchu,
-//         Profile: ProfileScreen,
-//     },
-// });
-
-// const Navigation = createStaticNavigation(Drawer);
-// export default function App() {
-//     return (
-// <NavigationContainer>
-//     <Drawer.Navigator initialRouteName="Home">
-//         <Drawer.Screen
-//             name="Home"
-//             component={Trangchu}
-//             options={{ headerShown: false }}
-//         />
-//         <Drawer.Screen
-//             name="Profile"
-//             component={ProfileScreen}
-//             options={{ headerShown: false }}
-//         />
-//         <Drawer.Screen
-//             name="Detail"
-//             component={Detail}
-//             options={{ headerShown: false }}
-//         />
-//     </Drawer.Navigator>
-// </NavigationContainer>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flexGrow: 1,
-//         backgroundColor: "#fff",
-//         justifyContent: "center",
-//     },
-// });
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
@@ -105,8 +48,12 @@ const HomePage = () => {
                 options={{ headerShown: false }}
             />
             <Drawer.Screen
-
                 name="Detail"
+                component={Detail}
+                options={{ headerShown: false }}
+            />
+            <Drawer.Screen
+                name="Admin"
                 component={AdminStack}
                 options={{ headerShown: false }}
             />
